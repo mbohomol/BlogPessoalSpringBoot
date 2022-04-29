700b12aa196538f66e19c05ddce42d94249b4aef
mbohomol <mbohomol@gmail.com>
2022-04-29 18:10:38 -0300

    Blog Pessoal

--- blogpessoal/src/main/java/com/generation/blogpessoal/PostagemController.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/PostagemController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,10 @@ public class PostagemController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
 	}
 	
+	@PutMapping
+	public ResponseEntity <Postagem> putPostagem(@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+	}
 	@DeleteMapping("/{id}")
 	public void deletePostagem(@PathVariable Long id) {
 		postagemRepository.deleteById(id);
