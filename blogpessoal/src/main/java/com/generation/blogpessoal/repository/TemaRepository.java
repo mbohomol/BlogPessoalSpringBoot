e08eb599ebc642f61a71059991ce2216d254afad
mbohomol <mbohomol@gmail.com>
2022-05-02 22:14:27 -0300

    Update TemaRepository.java

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -9,7 +9,6 @@ import com.generation.blogpessoal.model.Tema;
 
 @Repository // acessa o banco de dados e busca informações // interage com o banco de dados
 public interface TemaRepository extends JpaRepository <Tema, Long>{
-
 	 public List <Tema> findAllByDescricaoContainingIgnoreCase (String descricao);
 
 
