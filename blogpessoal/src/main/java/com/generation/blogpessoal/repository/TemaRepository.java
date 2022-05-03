6ce7a6f9d5ab4a7b2f5a1126d6d641e9b8e02aae
mbohomol <mbohomol@gmail.com>
2022-05-02 22:15:26 -0300

    Blog Pessoal

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.generation.blogpessoal.model.Tema;
 
 @Repository // acessa o banco de dados e busca informações // interage com o banco de dados
+
 public interface TemaRepository extends JpaRepository <Tema, Long>{
 	 public List <Tema> findAllByDescricaoContainingIgnoreCase (String descricao);
 
