2be9aa1f8f3153286de995df23cac482b2efd32f
mbohomol <mbohomol@gmail.com>
2022-05-02 22:27:41 -0300

    Blog Pessoal

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,12 +6,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity // create table // entidade = tabela
 @Table(name =  "tb_postagens") // nome da tabela: tb_postagens
@@ -29,31 +32,54 @@ public class Postagem {
     @Size(min= 10, max= 1000, message = "O atributo título deve conter no mínimo 5 e no máximo 100 caracteres ")
     private String texto; // atributo de "Postagens"
 
-    @UpdateTimestamp
-    private LocalDateTime data;  // métodos GET/SET(id/titulo/texto)
+    @UpdateTimestamp  //  recebe a data e hora do sistema
+    private LocalDateTime data;  
     
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getTexto() {
-        return texto;
-    }
-    public void setTexto(String texto) {
-        this.texto = texto;
-    }
-    public LocalDateTime getData() {
-        return data;
-    }
-    public void setData(LocalDateTime data) {
-        this.data = data;
-    }
+    // métodos GET/SET(id/titulo/texto)
+    
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+	private Tema tema;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+    
+   
 }
